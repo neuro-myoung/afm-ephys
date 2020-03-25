3617d2b515161dee84053c6d2e4f03e7faca1822
Mike Young <michael.young@duke.edu>
2020-03-25 18:49:13 -0400

    Fixed plot scale to be set by largest sweep in experiment and added add_scalelabs function.

--- plot_data.py
+++ plot_data.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import collections as mc
 import numpy as np
+from math import log10, floor
 
 
 class PlotData(object):
@@ -23,7 +24,7 @@ class PlotData(object):
                                  & (self.dat['ti'] <= self.end))]
         self.grps_sub = self.dat_sub.groupby('sweep')
 
-    def plot_sweep(self, sweep, scalebars=False):
+    def plot_sweep(self, sweep, scalebars=False, scalelabs=False):
         """
         This function will take as an argument a single sweep of pre-processed
         afm-ephys data and create a representative trace.
@@ -32,6 +33,8 @@ class PlotData(object):
         traces are stacked vertically and aligned on the time axis. By default
         there are no axis or labels save for the titles indicating the data
         shown in each plot.
+        By setting scalebars = True and/or scalelabs =True scalebars are and
+        labels are sized automatically and added to the plots.
         """
         self.plot_dat = self.grps_sub.get_group(sweep)
         self.fig, self.axs = plt.subplots(nrows=4, dpi=300, figsize=(2, 4),
@@ -45,10 +48,10 @@ class PlotData(object):
             ax.plot(self.plot_dat[var[0]], self.plot_dat[var[1]],
                     color=color, linewidth=0.5)
             ax.set_title(title, size=10)
-            self.plot_range = (np.max(self.plot_dat[var[1]])
-                               - np.min(self.plot_dat[var[1]]))
-            ax.set_ylim(np.min(self.plot_dat[var[1]]) - 0.05 * self.plot_range,
-                        np.max(self.plot_dat[var[1]]) + 0.05 * self.plot_range)
+            self.plot_range = self.axis_range(var[1])
+            self.plot_domain = self.axis_range(var[0])
+            ax.set_ylim(np.min(self.dat_sub[var[1]]) - 0.05 * self.plot_range,
+                        np.max(self.dat_sub[var[1]]) + 0.05 * self.plot_range)
             ax.axis('off')
 
             if scalebars is True:
@@ -56,31 +59,71 @@ class PlotData(object):
             else:
                 pass
 
+            if scalelabs is True:
+                self.add_scalelabs(ax, var[1])
+
         plt.tight_layout()
+        plt.savefig(self.fullpath + '_ex-trace.pdf', dpi=300)
         plt.show()
-        plt.savefig(self.fullpath + '_ex-trace.pdf', dpi=300,
-                    transparent=True)
 
     def add_scalebars(self, ax, var):
-        print(var)
+        """
+        This function adds scalebars to the trace axis when used.
+
+        By default it only adds a scalebar to the y-axis unless it is a current
+        vs. time plot. Scales are sized automatically.
+        """
+
+        ylen = self.round_1_sf(0.2*self.plot_range)
+
         if var == 'i_blsub':
-            [x, xend] = [np.min(self.plot_dat['ti']),
-                         np.min(self.plot_dat['ti']) + 100]
-            [y, yend] = [0.9 * np.max(self.plot_dat[var])-0.2*self.plot_range,
-                         0.9 * np.max(self.plot_dat[var])]
+            [x, xend] = [np.min(self.dat_sub['ti']),
+                         np.min(self.dat_sub['ti']) + 100]
+
+            [y, yend] = [0.9 * np.max(self.dat_sub[var])-ylen,
+                         0.9 * np.max(self.dat_sub[var])]
             lines = [[(x, y), (x, yend)],
                      [(x, y), (xend, y)]]
         else:
-            x = np.min(self.plot_dat['tin0'])
-            [y, yend] = [0.9 * np.max(self.plot_dat[var])-0.4*self.plot_range,
-                         0.9 * np.max(self.plot_dat[var])]
-            print(y)
-            print(yend)
-            lines = [[(x, y), (x, yend)]]
+            x = np.min(self.dat_sub['tin0'])
+            [y, yend] = [0.9 * np.max(self.dat_sub[var])-ylen*2,
+                         0.9 * np.max(self.dat_sub[var])]
 
+            lines = [[(x, y), (x, yend)]]
         lc = mc.LineCollection(lines, linewidths=0.5, colors='black')
         ax.add_collection(lc)
 
+    def add_scalelabs(self, ax, var):
+        """
+        This function adds labels to the scalebars when called.
+        """
 
-x = PlotData('example/test', (350, 1000))
-x.plot_sweep(10, scalebars=True)
+        lab_dict = {'i_blsub': ' pA', 'force': ' nN',
+                    'work': ' fJ', 'position': ' nm'}
+        ylen = self.round_1_sf(0.2*self.plot_range)
+
+        if var == 'i_blsub':
+            x1 = np.min(self.dat_sub['ti']) + 0.02 * self.plot_domain
+            y1 = 0.9 * np.max(self.dat_sub[var])-(ylen + 0.08*self.plot_range)
+            x2 = np.min(self.dat_sub['ti']) + 0.02 * self.plot_domain
+            y2 = 0.9 * np.max(self.dat_sub[var])-(ylen-0.02*self.plot_range)
+            ax.text(x1, y1, '100 ms', fontsize=6)
+            ax.text(x2, y2, str(int(ylen)) + lab_dict[var], fontsize=6)
+        else:
+            ylen *= 2
+            x1 = np.min(self.dat_sub['tin0']) + 0.02 * self.plot_domain
+            y1 = 0.9 * np.max(self.dat_sub[var])-(0.75*ylen)
+            ax.text(x1, y1, str(int(ylen)) + lab_dict[var], fontsize=6)
+
+    def round_1_sf(self, num):
+        """
+        This function will round a number to only 1 significant figure.
+        """
+        return(round(num, -int(floor(log10(abs(num))))))
+
+    def axis_range(self, var):
+        """
+        This function will find the range of a given plotting axis.
+        """
+        ax_range = (np.max(self.dat_sub[var]) - np.min(self.dat_sub[var]))
+        return(ax_range)
