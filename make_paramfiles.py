b4185c8038ee54cba67a002d9d1a3e93ae34d136
Mike Young <michael.young@duke.edu>
2020-03-27 16:18:45 -0400

    Moved some variables into the function definition, added annotation to the make_paramfiles function, and included a reference to an experiment uniqueID parameter.

--- make_paramfiles.py
+++ make_paramfiles.py
@@ -1,13 +1,27 @@
 import os
 import pandas as pd
 
-protocol = 'scan-80'
-rmstring = '_' + protocol + '.asc'
 keys = ['date', 'cell#',  'construct', 'protocol', 'nsweeps', 'velocity',
         'kcant', 'dkcant', 'Rp', 'Rm', 'Cm', 'Rs', 'Rscomp', 'mosm']
 
 
-def make_paramfiles(folder, keys=keys, rmstring=rmstring):
+def make_paramfiles(folder, protocol, keys=keys):
+    """
+    This function creates a file containing specific experimental parameters
+    that need to be input manually.
+
+    Arguments:
+        -folder: The path to a folder containing the files that need parameter
+                 files made.
+        -protocol: The specific protocol used in these experiments as a string
+                   so that it can be removed from the filename.
+        -keys: A list of keys for the parameter file. Default contains the
+               following:
+                date, cell#, construct, protocol, nsweeps, velocity, kcant,
+                dkcant, Rp, Rm, Cm, Rs, Rscomp, mosm
+
+    """
+    rmstring = '_' + protocol + '.asc'
     file_list = [f.replace(rmstring, '') for f in os.listdir(folder) if f.endswith('.' + 'asc')]
 
     for i in file_list:
@@ -20,6 +34,8 @@ def make_paramfiles(folder, keys=keys, rmstring=rmstring):
             input_str = input("Your input was missing values!")
         else:
             param_dict = {k: v for (k, v) in zip(keys, input_vals)}
-
+        param_dict['uniqueID'] = '-'.join(param_dict['date'],
+                                          param_dict['cell'],
+                                          param_dict['protocol'])
         x = pd.DataFrame.from_dict(param_dict, orient='index', columns=['val'])
         x.to_csv(folder + i + '_params.csv')
